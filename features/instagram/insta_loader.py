6173301e864b78ffedcbb30ad5b1f20a6e09e58e
YauheniPo <ip.popovich@mail.ru>
2020-01-03 01:34:43 +0300

    Added implementation of football matches site parsing

--- features/instagram/insta_loader.py
+++ features/instagram/insta_loader.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
+import logging
 import os
 
 import requests
@@ -8,18 +9,25 @@ from config import instagram_image_folder
 from features.instagram.insta_post import InstaPost
 from util.util_parsing import get_tree_html_content
 
+logger = logging.getLogger(__name__)
+
 
 def get_insta_post_data(post_content):
     tree_html_content = get_tree_html_content(post_content)
     image_utl = tree_html_content.xpath("//meta[@property='og:image']")[0].get('content')
-    post_description = json.loads(str(
-        tree_html_content.xpath("//script[@type='application/ld+json']//text()")[0]).strip()) \
-        .get('caption', 'This Instagram post does not a PUBLIC.')
-    return InstaPost(image_url=image_utl, post_description=post_description)
+    tree_post_description = tree_html_content.xpath("//script[@type='application/ld+json']//text()")
+    warning_msg = None
+    post_description = None
+    if not tree_post_description:
+        warning_msg = 'This Instagram post does not a PUBLIC.'
+    else:
+        post_description = json.loads(str(tree_post_description[0]).strip()) \
+            .get('caption', 'post without description')
+    return InstaPost(image_url=image_utl, post_description=post_description, warning=warning_msg)
 
 
 def fetch_insta_post_image(insta_post):
-    global f
+    f = None
     photo_name = insta_post.image_path
     try:
         requests_url = requests.get(insta_post.image_url)
@@ -27,5 +35,9 @@ def fetch_insta_post_image(insta_post):
             os.makedirs(instagram_image_folder)
         f = open(photo_name, 'ab')
         f.write(requests_url.content)
+    except:
+        logger.error(u"Save image ERROR: {}".format(photo_name))
+        insta_post.warning = 'Save image ERROR'
     finally:
-        f.close()
+        if f is not None:
+            f.close()
--- features/instagram/insta_post.py
+++ features/instagram/insta_post.py
@@ -7,10 +7,13 @@ from config import instagram_url_image_name_regexp, instagram_image_folder
 
 class InstaPost:
 
-    def __init__(self, image_url, post_description):
+    def __init__(self, image_url, post_description=None, warning=None):
         self.image_url = image_url
         self.post_description = post_description
-        post_image_name = post_description[:100] + re.search(instagram_url_image_name_regexp, image_url).group(0)
+        self.warning = warning
+        if post_description is not None:
+            post_image_name = post_description[:100] + re.search(instagram_url_image_name_regexp, image_url).group(0)
 
-        image_name = (lambda s: ''.join(['_' if c in [':', '"', ' ', ','] else c for c in s]))(post_image_name)
-        self.image_path = os.path.join(instagram_image_folder, image_name)
+            image_name = \
+                (lambda s: ''.join(['_' if c in [':', '"', ' ', ',', '?', '\n'] else c for c in s]))(post_image_name)
+            self.image_path = os.path.join(instagram_image_folder, image_name)
