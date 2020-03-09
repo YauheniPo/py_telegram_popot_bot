b301b542108463ac6787e859448d1a863265595e
YauheniPo <ip.popovich@mail.ru>
2020-03-09 04:11:09 -0700

    Update Currency Alarm script

--- popot_bot_actions.py
+++ popot_bot_actions.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import time
+from datetime import datetime
 
 import schedule
 from telegram import ParseMode
@@ -23,24 +24,33 @@ def job():
 
             bot.send_message(chat_id=user['id'],
                              text='<b>Currency Alarm</b>\n' + get_currency_message(currency_dollar_id),
-                             reply_markup=get_message_keyboard(button_currency_graph, actual_buttons_currency_selection, button_currency_alarm),
+                             reply_markup=get_message_keyboard(button_currency_graph, actual_buttons_currency_selection,
+                                                               button_currency_alarm),
                              parse_mode=ParseMode.HTML)
 
 
-schedule.every(12).seconds.do(job)
+def weekday_job(x, alert_time=None):
+    week = datetime.today().weekday()
+    if alert_time is not None and week < 5:
+        schedule.every().day.at(alert_time).do(x)
+
+
+weekday_job(job, '01:08')
+
+# schedule.every(12).seconds.do(job)
 # schedule.every().hour.do(job)
 # schedule.every().day.at("10:30").do(job)
 # schedule.every(5).to(10).minutes.do(job)
 # schedule.every().monday.do(job)
-# schedule.every().monday.thursday.wednesday.tuesday.friday.at("12:05").do(job)
-# schedule.every().minute.at(":17").do(job)
+# schedule.every().monday.thursday.wednesday.tuesday.friday.at("02:52").do(job)
+# schedule.every().days.at("02:55").do(job)
 
 if __name__ == "__main__":
     for i in range(0, 5):
-        time.sleep(60)
+        time.sleep(20)
         try:
             while True:
                 schedule.run_pending()
-                time.sleep(60)
+                time.sleep(20)
         except Exception as e:
             logger().error(e)
