33317dd8b97e507f8f2a4e4364556e4999e4bb66
Peter Evans <peter-evans@users.noreply.github.com>
2020-07-28 11:58:50 +0000

    autopep8 action fixes

--- test/test_base.py
+++ test/test_base.py
@@ -12,5 +12,6 @@ def test_bot_script_func():
     bot_script_all = popot_bot.__all__
     with open(path.join(popot_bot.__file__), "r") as script:
         script_content = script.read()
-    actual_bot_script_func = re.findall(ROBOT_SCRIPT_FUNC_NAME_REGEXP, script_content)
+    actual_bot_script_func = re.findall(
+        ROBOT_SCRIPT_FUNC_NAME_REGEXP, script_content)
     assert_that(bot_script_all, contains_inanyorder(*actual_bot_script_func))
