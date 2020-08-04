95d6a013a51dc411f2317d233745556d45e37061
YauheniPo <ip.popovich@mail.ru>
2020-08-04 23:51:23 +0300

    Used singleton for DB connection and fixed tests

--- features/instagram/insta_loader.py
+++ features/instagram/insta_loader.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
+import datetime
+import os
+
+from selenium.common.exceptions import NoSuchElementException
+
 from bot_config import instagram_save_content_service
 from util.logger import logger
-from util.webdriver_helper import WebDriverFactory, wait_for_ajax, wait_visibility, FIREFOX
+from util.webdriver_helper import WebDriverFactory, wait_for_ajax, wait_visibility, take_screenshot, FIREFOX
 
 
 def fetch_insta_post_data(insta_post_model):
@@ -10,15 +15,19 @@ def fetch_insta_post_data(insta_post_model):
     results_for_download = f"{searching_result}//a[@download]"
 
     with WebDriverFactory(FIREFOX).get_webdriver_instance() as driver:
-        driver.get(instagram_save_content_service + insta_post_model.post_url)
-        wait_visibility(True, driver, searching_result)
-        wait_for_ajax(driver)
-        insta_post_items = driver.find_elements_by_xpath(results_for_download)
-        post_media_urls = [media.get_attribute(
-            "href") for media in insta_post_items]
-        insta_post_model.media_urls = post_media_urls
-        insta_post_description = driver.find_element_by_xpath(
-            insta_media_description_xpath).text
-        insta_post_model.post_description = insta_post_description
+        try:
+            driver.get(instagram_save_content_service + insta_post_model.post_url)
+            wait_visibility(True, driver, searching_result)
+            wait_for_ajax(driver)
+            insta_post_items = driver.find_elements_by_xpath(results_for_download)
+            post_media_urls = [media.get_attribute(
+                "href") for media in insta_post_items]
+            insta_post_model.media_urls = post_media_urls
+            insta_post_description = driver.find_element_by_xpath(
+                insta_media_description_xpath).text
+            insta_post_model.post_description = insta_post_description
+        except NoSuchElementException as ex:
+            take_screenshot(driver, os.path.join("screenshot", f"{datetime.datetime.now().microsecond}.jpg"))
+            raise ex
 
     logger().info(("--Instagram instance-- '{}'".format(insta_post_model.__dict__)).encode("utf-8"))
